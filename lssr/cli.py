a1a87cb82c2b695b02646dc359fc470c811929f4
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-07-07 01:14:03 +0900

    Add print_version

--- lssr/cli.py
+++ lssr/cli.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-from . import core
+from . import __version__, core
 
 
 def main(args: list[str]) -> None:
+    if "--version" in args:
+        print(__version__)
+        return
     core.main(args)
